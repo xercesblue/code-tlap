297650e8d3e7110b670c3abca0e2b8fc632ee2f3
dlcs.frank <dlcs.frank@gmail.com>
2013-06-22 11:23:56 -0700

    Added tlap-3-arrays examples.
    Added tlap-3-arrays examples.

---
+++ src/tlap-3-arrays/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+
+#include <tlap-lib.h>
+
+
+
+int mode(int n[], const int N_SIZE) {
+	print_arr(n, N_SIZE);
+
+	// Generate Histogram
+	int *hist = new int[N_SIZE];
+	for (int i = 0; i < N_SIZE; ++i) hist[i] = 0;
+
+	for (int i = 0; i < N_SIZE; ++i) hist[n[i]]++;
+
+	int max_freq = 0;
+	for (int i = 0; i < N_SIZE; ++i) {
+			if (hist[i] > hist[max_freq]) max_freq = i;
+	}
+
+	delete[] hist;
+	return max_freq;
+}
+
+int main(int argc, char* argv[]) {
+
+	const int N_SIZE = 11;
+
+	int n[N_SIZE] = {1,2,3,4,5,6,6,7,8,9,0};
+	int mode_n = mode(n, N_SIZE);
+	print_arr(n, N_SIZE);
+	std::cout << "Mode is: " << mode_n << std::endl;
+	return 0;
+}
