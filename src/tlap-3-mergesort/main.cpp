9cd46bb4f40cae05d81ae8a9a70643486f355f3a
dlcs.frank <dlcs.frank@gmail.com>
2013-06-25 17:58:41 -0700

    tlap-3-mergesort uses std::list now

--- src/tlap-3-mergesort/main.cpp
+++ src/tlap-3-mergesort/main.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
-#include <queue>
+#include <list>
 #include <tlap-lib.h>
 
 void merge_h_(int n[], int low, int half, int high) {
-	std::queue<int> b1, b2;
+	std::list<int> b1, b2;
 
-	for (int i = low; i <= half; ++i) b1.push(n[i]);
-	for (int i = half + 1; i <= high; ++i) b2.push(n[i]);
+	for (int i = low; i <= half; ++i) b1.push_back(n[i]);
+	for (int i = half + 1; i <= high; ++i) b2.push_back(n[i]);
 
 	int i = low, eb1 = 0, eb2 = 0;
 
 	while (!b1.empty() && !b2.empty()) {
 		eb1 = b1.front(); eb2 = b2.front();
 		if (eb1 < eb2) {
-			b1.pop();
+			b1.pop_front();
 			n[i++] = eb1;
 		} else {
-			b2.pop();
+			b2.pop_front();
 			n[i++] = eb2;
 		}
 	}
 	// Add remaining elements
-	while (!b1.empty()) { n[i++] = b1.front(); b1.pop(); }
-	while (!b2.empty()) { n[i++] = b2.front(); b2.pop(); }
+	while (!b1.empty()) { n[i++] = b1.front(); b1.pop_front(); }
+	while (!b2.empty()) { n[i++] = b2.front(); b2.pop_front(); }
 }
 
 
@@ -43,18 +43,17 @@ void mergesort_h_(int n[], int start, int end) {
 // Merge left and right arrays
 void mergesort(int n[], const int N_SIZE) {
 	if (N_SIZE < 1) return;
-	print_arr(n, N_SIZE);
 	mergesort_h_(n, 0, N_SIZE - 1);
-	std::cout << "=======" << std::endl;
-	print_arr(n, N_SIZE);
 }
 
-
-
-
 int main(int argc, char* argv[]) {
-	const int N_SIZE = 10;
-	int n[N_SIZE] = { 9,8,7,6,5,4,3,2,1,0 };
-	mergesort(n, N_SIZE);
+	const int N_SIZE = 10000;
+	ArrayGenerator ag;
+	int* arr = ag.gen_random(N_SIZE);
+	Timer t;
+	t.start();
+	mergesort(arr, N_SIZE);
+	t.stop();
+	std::cout << t.elapsedMS().count() << "ms" << std::endl;
 	return 0;
 }
