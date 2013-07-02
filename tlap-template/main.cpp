0331a260a76bd418dc046629282fb021e14b51f3
dlcs.frank <dlcs.frank@gmail.com>
2013-07-02 11:38:05 -0700

    Added tlap-template from previous project hierarchy.

---
+++ tlap-template/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// Francisco De La Cruz
+// dlcs.frank@gmail.com
+// <DATE>
+
+// See cpp/src/tlap-lib/tlap-lib.h/.cpp for up to date code.
+
+int main(int argc, char* argv[]) {
+
+
+	return 0;
+}
