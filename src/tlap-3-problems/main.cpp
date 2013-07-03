bfc9f4fd6ce4efbdd39098ac9571fdb81a206dd8
dlcs.frank <dlcs.frank@gmail.com>
2013-07-02 23:40:13 -0700

    Added substring() and replace() to tlap-3-problems.

--- src/tlap-3-problems/main.cpp
+++ src/tlap-3-problems/main.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include <cstring>
 #include <cmath>
+#include <stdexcept>
 #include <tlap-lib.h>
 
 // Francisco De La Cruz
 // dlcs.frank@gmail.com
 // July 2, 2013
 
+// Many of the examples below are *very* c-like.
+// You've been warned :)
+
 // See cpp/src/tlap-lib/tlap-lib.(h|cpp) for up to date code.
 
 typedef char* ArrayString;
+typedef const char* ArrayStringC;
 
 /**	Note: Expects null-terminated strings.
  * @brief length calculates length of string
  * @param str string to calculate length for
  * @return length of str
  */
-int length(const ArrayString str) {
+int length(ArrayString str) {
+	if (!str) return 0;
+
+	int l = 0;
+	while(str[l] != '\0') ++l;
+	return l;
+}
+
+int length(ArrayStringC str) {
 	if (!str) return 0;
 
 	int l = 0;
@@ -45,6 +58,69 @@ void append_ch(ArrayString& str, char ch) {
 
 	str = str2;
 
+}
+
+/**
+ * @brief substring get substring str[start..start+len)
+ * @param str string to operate on
+ * @param N_SIZE size of str
+ * @param start starting position (>= 1)
+ * @param len length of return substring
+ * @return substring to return
+ */
+ArrayString substring(ArrayString str, int start, int mlen) {
+	int str_len = length(str);
+	if (str_len < 1 || mlen > str_len || start > mlen || start < 1) throw std::invalid_argument("Invalid arguments to substring");
+
+	ArrayString sub = new char[mlen + 1];
+	for (int i = start - 1; i < start - 1 + mlen; ++i) {
+		sub[i - start + 1] = str[i];
+	}
+	sub[str_len] = '\0';
+
+	return sub;
+}
+
+
+bool equals_upto(ArrayStringC str, ArrayStringC str2, const int start_pos, const int upto) {
+	if (!str || !str2) throw std::invalid_argument("equal_upto:: Empty strings");
+	int len1 = length(str), len2 = length(str2);
+	if (len1 < len2 || upto > len2) throw std::invalid_argument("equal_upto:: str must be larger than str2. Upto must be <= length(str2)");
+	if (start_pos >= len1 || start_pos < 0) throw std::invalid_argument("equal_upto:: start_pos must be within [0, length(str))");
+
+	int i = start_pos;
+
+	while (i - start_pos < upto && i - start_pos < len2 && str[i] == str2[i - start_pos]) i++;
+
+	return i == start_pos + upto;
+}
+
+int find(ArrayStringC str, ArrayStringC what, const int start_pos = 0) {
+
+	int w_len = length(what);
+	int str_len = length(str);
+	int pos = start_pos;
+	while (pos < str_len && !equals_upto(str, what, pos, w_len)) pos++;
+
+	return pos < str_len ? pos : -1;
+}
+
+void replace(ArrayString str, ArrayStringC this_str, ArrayStringC that_str) {
+	int str_len = length(str);
+	int this_len = length(this_str);
+	int that_len = length(that_str);
+	if (this_len != that_len) throw std::invalid_argument("Replace only works on same length strings.");
+	int last_match = 0, match = 0;
+
+	while( last_match < str_len ) {
+		match = find(str, this_str, last_match);
+		if (match != -1) {
+			last_match = match + that_len;
+			for (int j = 0; j < that_len; ++j) str[j + match] = that_str[j];
+		} else {
+			last_match += that_len;
+		}
+	}
 }
 
 double median(int n[], const int N_SIZE) {
@@ -102,13 +178,20 @@ int main(int argc, char* argv[]) {
 	const int SCORES_SIZE = 11;
 	int scores[11] = { 10, 80, 78, 45, 99, 100, 60, 65, 78, 80, 84 };
 
-	ArrayString str = new char[4];
-	ArrayString strnull = nullptr;
 
-	std::strncpy(str, "cool", 4);
+	ArrayString str = new char[5];
+	ArrayString strnull = nullptr, strsub = nullptr;
+	ArrayString substr = new char[8];
+	ArrayString strrep = new char[8];
+
+	std::strncpy(str, "cool", 5);
+	std::strncpy(substr, "abcdefg", 8);
+	std::strncpy(strrep, "abcabfg", 8);
 
 	append_ch(str, '!');
 	append_ch(strnull, '?');
+	strsub = substring(substr, 1, 7);
+	replace(strrep, "ab", "XX");
 
 	std::cout << "Median of: ";	print_arr(arr_unsorted, N_SIZE);
 	std::cout << median(arr_unsorted, N_SIZE) << std::endl;
@@ -116,8 +199,15 @@ int main(int argc, char* argv[]) {
 	std::cout << "Appending '?' to null string: " << strnull << std::endl;
 	std::cout << "Scores: "; print_arr(scores, SCORES_SIZE);
 	std::cout << "25 percentile of Scores: " << percentile(scores, SCORES_SIZE, 100) << std::endl;
+	std::cout << "Test substring(1, 7) on 'abcdefg': " << strsub << std::endl;
+	std::cout << "Test replace(\"ab\",\"XX\") on 'abcabfg': " << strrep << std::endl;
+
+
 	delete[] str;
 	delete[] strnull;
+	delete[] strsub;
+	delete[] substr;
+	delete[] strrep;
 
 	return 0;
 }
